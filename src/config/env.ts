e81ef9cf6a657e5be80f742bd49dcddeaded3fd3
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2023-12-31 20:05:01 +0200

    export evn variables

---
+++ src/config/env.ts
@@ -0,0 +1,3 @@
+export const TELEGRAM_API_KEY = process.env.NEXT_PUBLIC_TELEGRAM_API_KEY!;
+export const TELEGRAM_API_URL = process.env.NEXT_PUBLIC_TELEGRAM_API_URL!;
+export const TELEGRAM_CHAT_ID = process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID!;
