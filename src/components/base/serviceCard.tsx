533e88f0a6da853d26d63d6c6a03dc8545e6b618
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2023-12-31 20:44:11 +0200

    add scroll on click

--- src/components/base/serviceCard.tsx
+++ src/components/base/serviceCard.tsx
@@ -18,12 +18,14 @@ export default function ServiceCard({
     <div className="group w-full transition duration-300 hover:!opacity-100 group-hover/list:opacity-20 lg:w-1/2">
       <div className="m-3 flex flex-col items-start gap-3 rounded-lg p-4 ">
         <div className="text-xl">{type}</div>
-        <div className="text-3xl font-medium text-slate-50">{price}</div>
-        <p className="text-start text-sm text-slate-400">
+        <div className="text-3xl font-medium text-slate-900 dark:text-slate-50">
+          {price}
+        </div>
+        <p className="text-start text-sm text-slate-500 dark:text-slate-400">
           Whether you want a web app, paired with a marketing website, or a
           cross-platform app, I got you covered.
         </p>
-        <div className="flex flex-col gap-2.5 text-start">
+        <div className="mt-3 flex flex-col gap-2.5 text-start">
           <ServiceItem>Unique, modern design</ServiceItem>
           <ServiceItem>Responsive layout for all device screens</ServiceItem>
           <ServiceItem>Optimized performance</ServiceItem>
@@ -34,7 +36,15 @@ export default function ServiceCard({
           </ServiceItem>
         </div>
         <div className="mt-5 flex w-full justify-center">
-          <Button>Get the package</Button>
+          <Button
+            onClick={() =>
+              document
+                .getElementById("contactme")
+                ?.scrollIntoView({ behavior: "smooth" })
+            }
+          >
+            Get the package
+          </Button>
         </div>
       </div>
     </div>
@@ -58,7 +68,7 @@ function ServiceItem({
           sx={{ color: "rgb(148 163 184)" }}
         />
       )}
-      <div className="text-slate-400">{children}</div>
+      <div className="text-slate-600 dark:text-slate-400">{children}</div>
     </div>
   );
 }
