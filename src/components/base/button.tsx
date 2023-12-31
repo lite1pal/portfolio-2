c598806b59183d67e16c1c1214ec873cfc9e0527
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2023-12-31 20:31:12 +0200

    add type prop

--- src/components/base/button.tsx
+++ src/components/base/button.tsx
@@ -1,15 +1,23 @@
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
+
+interface IProps {
+  className?: string;
+  type?: "button" | "submit";
+  onClick?: () => void;
+  children: ReactNode;
+}
 
 export default function Button({
   className,
+  type = "button",
+  onClick,
   children,
-}: {
-  className?: string;
-  children: ReactNode;
-}) {
+}: IProps) {
   return (
     <button
-      className={`${className} duration-50 w-fit rounded border-2 border-emerald-400 bg-emerald-400 px-4 py-2 text-lg text-slate-50 transition hover:bg-transparent hover:text-emerald-400 dark:text-slate-900 dark:hover:bg-slate-900`}
+      type={type}
+      onClick={onClick}
+      className={`${className} duration-50 bg-primary border-primary dark:bg-primary-dark dark:border-primary-dark hover:text-primary dark:hover:text-primary-dark w-fit rounded border-2 px-4 py-2 text-lg text-slate-50 transition hover:bg-transparent dark:text-slate-900 dark:hover:bg-slate-900`}
     >
       {children}
     </button>
