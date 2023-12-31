d4d8e0e170431daea0e995806afadbf21acc4733
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2023-12-31 20:48:38 +0200

    add custom span component

--- src/components/home/components/services.tsx
+++ src/components/home/components/services.tsx
@@ -2,19 +2,20 @@ import SectionHeader from "@/components/base/sectionHeader";
 import SectionParagraph from "@/components/base/sectionParagraph";
 import SectionTitle from "@/components/base/sectionTitle";
 import ServiceCard from "@/components/base/serviceCard";
+import Span from "@/components/base/span";
 
 export default function Services() {
   return (
-    <div className="flex flex-col items-center gap-10">
+    <div id="services" className="flex flex-col items-center gap-10">
       <div className="flex flex-col items-center gap-2 text-center">
         <SectionTitle>Services</SectionTitle>
         <SectionHeader>Your idea, my implementation</SectionHeader>
         <SectionParagraph>
           I know you have many great ideas lying dusting on the shelf in the
           backyard shed.{" "}
-          <span className="text-slate-100">
+          <Span>
             Bring them to me, and let{"'"}s discuss how to make them shine
-          </span>
+          </Span>
         </SectionParagraph>
         <div className="group/list mt-3 flex w-full flex-col flex-wrap gap-0 lg:flex-row">
           <ServiceCard type="Blog" price="900 $" />
