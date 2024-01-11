5c458de376f225703f41d2b1ea33491bc1de17db
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2024-01-11 20:54:39 +0200

    add og meta to blog route

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,6 +4,10 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Blog",
   description: "Read about my journey in the web dev, design and more.",
+  openGraph: {
+    title: "Blog",
+    description: "Read about my journey in the web dev, design and more.",
+  },
 };
 
 export default function Blog() {
